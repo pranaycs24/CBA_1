0ede430550b91c41325f533e63b8785e0e3db499
pranaykumarsaha <pranay.kanua@gmail.com>
2022-05-03 07:36:14 +0530

    Fetch Data Complete With RecyclerView

---
+++ app/src/main/java/com/can/book/anything/admin/DataFetchAdapter.java
@@ -0,0 +1,71 @@
+package com.can.book.anything.admin;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+
+public class DataFetchAdapter extends FirestoreRecyclerAdapter<PersonalData, DataFetchAdapter.DataFetchHolder> {
+
+    private Context context;
+
+    public DataFetchAdapter(@NonNull FirestoreRecyclerOptions<PersonalData> options, Context context) {
+        super(options);
+        this.context = context;
+    }
+
+    @Override
+    protected void onBindViewHolder(@NonNull DataFetchHolder holder, int position, @NonNull PersonalData model) {
+        holder.texViewName.setText(model.getName());
+        holder.textViewAdd.setText(model.getAddress());
+        holder.buttonCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String phoneNumber = model.getPhoneNumber();
+//                Toast.makeText(context, model.getPhoneNumber(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(Intent.ACTION_DIAL);// Uri.parse(model.getPhoneNumber(), phone, ))
+                intent.setData(Uri.parse("tel:" + phoneNumber));
+                context.startActivity(intent);
+//                if (intent.resolveActivity(context.getPackageManager()) != null) {
+//                    Toast.makeText(context, "TEL", Toast.LENGTH_SHORT).show();
+//
+//                }
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public DataFetchHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.data_info, parent, false);
+        return new DataFetchHolder(v);
+    }
+
+    class DataFetchHolder extends RecyclerView.ViewHolder{
+        TextView texViewName;
+        TextView textViewAdd;
+        ImageButton buttonCall;
+
+        public DataFetchHolder(@NonNull View itemView) {
+            super(itemView);
+            texViewName  = itemView.findViewById(R.id.text_view_title);
+            textViewAdd = itemView.findViewById(R.id.text_view_address);
+            buttonCall = itemView.findViewById(R.id.button_call);
+
+        }
+    }
+
+}
--- app/src/main/java/com/can/book/anything/admin/FetchData.java
+++ app/src/main/java/com/can/book/anything/admin/FetchData.java
@@ -2,18 +2,23 @@ package com.can.book.anything.admin;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
@@ -23,6 +28,7 @@ public class FetchData extends AppCompatActivity {
     private static final String TAG = "FetchData_1";
     private CollectionReference notebookRef;
     private ListenerRegistration noteListener;
+    private DataFetchAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,44 +53,62 @@ public class FetchData extends AppCompatActivity {
 //            }
 //        });
 
+        setUpRecycleView();
+
+    }
+    private void setUpRecycleView() {
+        Intent intent = getIntent();
+        String from = intent.getStringExtra("from");
+        textView.setText(from);
+        notebookRef = db.collection(from);
+        Query query = notebookRef.orderBy("serviceArea", Query.Direction.ASCENDING);
+        FirestoreRecyclerOptions<PersonalData> options = new FirestoreRecyclerOptions.Builder<PersonalData>()
+                .setQuery(query, PersonalData.class)
+                .build();
+        adapter = new DataFetchAdapter(options, FetchData.this);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Intent intent = getIntent();
-        String from = intent.getStringExtra("from");
-        notebookRef = db.collection(from);
-        noteListener = notebookRef.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-                if (error != null) {
-                    return;
-                }
-
-                String s = "";
-
-                for (QueryDocumentSnapshot documentSnapshot : value) {
-                    PersonalData note = documentSnapshot.toObject(PersonalData.class);
-                    note.setDocumentId(documentSnapshot.getId());
-
-                    String documentId = note.getDocumentId();
-                    String name = note.getName();
-                    String phone = note.getPhoneNumber();
-                    String serviceArea = note.getServiceArea();
-                    String address = note.getAddress();
-                    s += name +"\n" +phone+"\n" + address + "\n" +serviceArea+ "\n\n";
-                    Log.d(TAG, s);
-                }
-
-                textView.setText(s);
-            }
-        });
+        adapter.startListening();
+//        noteListener = notebookRef.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+//            @Override
+//            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+//                if (error != null) {
+//                    return;
+//                }
+//
+//                String s = "";
+//
+//                for (QueryDocumentSnapshot documentSnapshot : value) {
+//                    PersonalData note = documentSnapshot.toObject(PersonalData.class);
+//                    note.setDocumentId(documentSnapshot.getId());
+//
+//                    String documentId = note.getDocumentId();
+//                    String name = note.getName();
+//                    String phone = note.getPhoneNumber();
+//                    String serviceArea = note.getServiceArea();
+//                    String address = note.getAddress();
+//                    s += name +"\n" +phone+"\n" + address + "\n" +serviceArea+ "\n\n";
+//                    Log.d(TAG, s);
+//                }
+//
+//                textView.setText(s);
+//            }
+//        });
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        noteListener.remove();
+//        noteListener.remove();
+        adapter.startListening();
     }
 }
